da9d41047d49cf0792065b58fe30b13944015797
adnatecodez <adeaduratimilehin@gmail.com>
2025-09-02 19:50:05 +0100

    configured server port

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact(), tailwindcss()],
+  server: {
+    port: parseInt(process.env.PORT) || 5173
+  }
 });
