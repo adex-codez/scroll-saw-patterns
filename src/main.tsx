0c0ae1771f7e7423f55bdcdc24f51fa88060ee1e
adnatecodez <adeaduratimilehin@gmail.com>
2026-06-16 05:29:44 +0100

    fixed input hanging

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,8 @@
 import ReactDOM from 'react-dom/client'
-
-const rootElement = document.getElementById('app')!
-
 import { RouterProvider } from '@tanstack/react-router'
 import { createRouter } from './router'
-const router = createRouter()
 
-if (!rootElement.innerHTML) {
-  const root = ReactDOM.createRoot(rootElement)
-  root.render(<RouterProvider router={router} />)
-}
+const router = createRouter()
+const rootElement = document.getElementById('app')!
+const root = ReactDOM.createRoot(rootElement)
+root.render(<RouterProvider router={router} />)
